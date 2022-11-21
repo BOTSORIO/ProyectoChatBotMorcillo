db998bec2918256d51a31d990e36e1c3be34c718
BOTSORIO <sebastian.quinteroo@uqvirtual.edu.co>
2022-11-21 01:13:29 -0500

    Morcillo terminado

--- mainBot.py
+++ mainBot.py
@@ -10,13 +10,13 @@ import json
 import random
 import pickle # Guardar el modelo json
 
-nltk.download('punkt') #Validación - Descarga paquete en caso de que moleste al ejecutarlo
+#nltk.download('punkt') #Validación - Descarga paquete en caso de que moleste al ejecutarlo
 
-with open('./ProyectoChatBotMorcillo/contenido.json', encoding='utf-8') as archivo:
+with open('./data/contenido/contenido.json', encoding='utf-8') as archivo:
     datos = json.load(archivo)
 
 try:
-    with open('./ProyectoChatBotMorcillo/variables.pickle','rb') as archivoPickle:
+    with open('./data/archivos/variables.pickle','rb') as archivoPickle:
         palabras, tags, entrenamiento, salida = pickle.load(archivoPickle)
 except:
 
@@ -60,7 +60,7 @@ except:
 
     entrenamiento = numpy.array(entrenamiento)
     salida = numpy.array(salida)
-    with open('./ProyectoChatBotMorcillo/variables.pickle','wb') as archivoPickle:
+    with open('./data/archivos/variables.pickle','wb') as archivoPickle:
         pickle.dump((palabras, tags, entrenamiento, salida), archivoPickle)
 
 tensorflow.compat.v1.reset_default_graph()
@@ -73,16 +73,14 @@ red = tflearn.regression(red)
 
 modelo = tflearn.DNN(red)
 
-#Si se quita es try demora mas-repite el entrenamiento
-# try:
-#      modelo.load('modelo.tflearn')
-# except:
 modelo.fit(entrenamiento, salida, n_epoch= 1000, batch_size=10, show_metric=True) #Va a ver la información 1000 veces y cuantas entradas
-modelo.save('./ProyectoChatBotMorcillo/modelo.tflearn')
+modelo.save('./data/archivos/modelo.tflearn')
 
 def mainBot(texto):   
     if texto!= '' and texto != ' ':
-        chatLog.insert(END, "Tu: " + texto + '\n\n')
+        chatLog.insert(END, "    ")
+        chatLog.image_create(END, image = imgRU, pady=20)
+        chatLog.insert(END, "  "+texto + '\n')
 
         cubeta = [0 for _ in range(len(palabras))]
         entradaProcesada = nltk.word_tokenize(texto)
@@ -101,7 +99,9 @@ def mainBot(texto):
             if tagAux['tag'] == tag:
                 respuesta = tagAux['respuestas']
 
-        chatLog.insert(END, "Morcillo: " + random.choice(respuesta) + '\n\n')
+        chatLog.insert(END, "    ")
+        chatLog.image_create(END, image = imgRB)
+        chatLog.insert(END, "  "+random.choice(respuesta) + '\n\n')
 
         if texto == 'Adios' or texto == 'Hasta la proxima' or texto == 'Chao' or texto == 'adios' or texto == 'chao' or texto == 'hasta la proxima':
             root.destroy()
@@ -111,21 +111,27 @@ def mainBot(texto):
 # Interfaz grafica
 root = Tk()
 root.title("ChatBot")
-root.geometry("400x400")
+root.geometry("600x600")
 root.resizable(width=False, height=False)
-chatLog = Text(root, bd=0, bg="white", height="8", width="50", font="Arial")
+
+img = PhotoImage(file='./data/img/morcillo.png')
+imgRB = PhotoImage(file='./data/img/morcillo_respuesta.png')
+imgRU = PhotoImage(file='./data/img/user.png')
+
+chatLog = Text(root, bd=0, bg="white", height="10", width="50", font="Helvetica", pady=10, )
 scrollbar = Scrollbar(root, command=chatLog.yview, cursor="heart")  
 chatLog['yscrollcommand'] = scrollbar.set
-chatLog.config(foreground="#442265", font=("Verdana", 12 ))
+chatLog.config(foreground="#2C3E50", font=("Verdana", 12 ))
 chatLog.pack(expand= 1, fill= BOTH)
 frame = Frame(root)
 frame.pack(fill="x")
-Label(frame, text="Comenta lo que necesitas al bot", font="Verdana 10").pack(anchor="center", pady=10)
+Label(frame, image=img).pack(side=TOP, pady=10)
+Label(frame, text="Comenta lo que necesitas al bot", font=("Helvetica", 12, "bold")).pack(anchor="center", pady=10)
 texto = StringVar()
 entradaTexto = Entry(frame)
 entradaTexto.config(bd=5, font=("Verdana", 10), width=50, textvariable=texto)
 entradaTexto.pack(padx=20)
-botonEnviar = Button(frame, text="Enviar")
-botonEnviar.config(bd=3, font=("Verdana", 10), command=lambda:mainBot(texto.get()))
+botonEnviar = Button(frame, text="Enviar", font= 'currier 13 bold', width= 15 , fg= '#17202A', bg= '#A6B2B3', highlightbackground= 'black', highlightthickness = 2)
+botonEnviar.config(bd=3, font=("Helvetica", 10, "bold"), command=lambda:mainBot(texto.get()))
 botonEnviar.pack(pady=10)
 root.mainloop()
